85167845b401b0ba986da92b3a98723b1bfd260d
Tu Jobs <nguyenvantu.epu@gmail.com>
2017-06-10 16:32:16 +0700

    how can use i18n?
    oh yeah
    
    The End
    
    i18n application
    
    it's ok
    
    arigatou
    
    let's merged

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+ def show
+    if valid_page?
+      render template: "static_pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
   def home
   end
 
@@ -9,5 +17,11 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  private
+
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/static_pages/#{params[:page]}.html.erb"))
   end
 end

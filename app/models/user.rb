f4f0672ca50566011b1b4364b7edde23fe8cc310
Tu Jobs <nguyenvantu.epu@gmail.com>
2017-06-16 15:11:14 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
-class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
 
-  before_save   :downcase_email
+  before_save   {email.downcase!}
   before_create :create_activation_digest
 
   validates :name, presence: true,
@@ -47,21 +47,32 @@ class User < ApplicationRecord
     self == user
   end
 
-  def downcase_email
-    self.email = email.downcase
-  end
-
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest activation_token
-  end
-
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
   def activate
     update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.user.gi.hours.ago
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 
   has_secure_password

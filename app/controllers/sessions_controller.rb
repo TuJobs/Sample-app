de9dbc523dee988785390cb3da46de5274aab54d
Tu Jobs <nguyenvantu.epu@gmail.com>
2017-06-16 15:11:14 +0700

    Add account activationPause

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
+
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        "1" == params[:session][:remember_me] ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = t ".account_not_activated. "
+        message += t ".check_mail"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t ".invalid_email_pass"
       render :new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 User.create! name: "Example User", email: "example@railstutorial.org",
-  password: "19041996", password_confirmation: "19041996", is_admin: true
+  password: "19041996", password_confirmation: "19041996",
+  is_admin: true, activated: true, activated_at: Time.zone.now
 
 Settings.db.slot.times do |n|
   name = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   password = "password"
   User.create! name: name, email: email, password: password,
-    password_confirmation: password
+    password_confirmation: password, activated: true, activated_at: Time.zone.now
 end
